e9bfa112a8c53a3740b3a01046442047f16ad978
Will Sargent <will.sargent@gmail.com>
2018-07-28 09:47:15 -0700

    Update main (#3)
    * Update main
    
    * Cleanup

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,16 +2,11 @@ import com.tersesystems.debugjsse.AbstractDebug;
 import com.tersesystems.debugjsse.Debug;
 import com.tersesystems.debugjsse.DebugJSSEProvider;
 import org.slf4j.LoggerFactory;
-import sun.security.tools.keytool.CertAndKeyGen;
-import sun.security.x509.X500Name;
 
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.X509ExtendedKeyManager;
 import javax.net.ssl.X509ExtendedTrustManager;
-import java.io.IOException;
-import java.security.*;
-import java.security.cert.CertificateException;
+import java.security.KeyStore;
 import java.security.cert.X509Certificate;
 import java.util.Arrays;
 
@@ -38,7 +33,8 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         DebugJSSEProvider.enable().setDebug(slf4jDebug);
-        KeyStore ks = emptyStore();
+        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+        ks.load(null, "changeit".toCharArray());
 
         TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
         trustManagerFactory.init(ks);
@@ -49,24 +45,25 @@ public class Main {
         System.out.println("trustManager = " + Arrays.toString(acceptedIssuers));
     }
 
-    private static KeyStore emptyStore() throws Exception {
-        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
-        ks.load(null, "changeit".toCharArray());
-
-        long validSecs = (long) 365 * 24 * 60 * 60;
-        CertAndKeyGen certGen = generateCertificate();
-        X509Certificate x509Certificate =
-        certGen.getSelfCertificate(
-                new X500Name("CN=My Application,O=My Organisation,L=My City,C=DE"), validSecs);
-        ks.setKeyEntry("mycert", certGen.getPrivateKey(), "changeit".toCharArray(),
-                new X509Certificate[] { x509Certificate });
-
-        return ks;
-    }
-
-    static CertAndKeyGen generateCertificate() throws Exception {
-        CertAndKeyGen certGen = new CertAndKeyGen("RSA", "SHA256WithRSA", null);
-        certGen.generate(2048);
-        return certGen;
-    }
+    //
+    //    private static KeyStore emptyStore() throws Exception {
+    //        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+    //        ks.load(null, "changeit".toCharArray());
+    //
+    //        long validSecs = (long) 365 * 24 * 60 * 60;
+    //        CertAndKeyGen certGen = generateCertificate();
+    //        X509Certificate x509Certificate =
+    //        certGen.getSelfCertificate(
+    //                new X500Name("CN=My Application,O=My Organisation,L=My City,C=DE"), validSecs);
+    //        ks.setKeyEntry("mycert", certGen.getPrivateKey(), "changeit".toCharArray(),
+    //                new X509Certificate[] { x509Certificate });
+    //
+    //        return ks;
+    //    }
+    //
+    //    static CertAndKeyGen generateCertificate() throws Exception {
+    //        CertAndKeyGen certGen = new CertAndKeyGen("RSA", "SHA256WithRSA", null);
+    //        certGen.generate(2048);
+    //        return certGen;
+    //    }
 }
