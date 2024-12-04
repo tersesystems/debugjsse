6ac68b4d7d53a584ae0f03ca4919d9a8dee5cad7
Will Sargent <will.sargent@gmail.com>
2024-12-03 20:46:50 -0800

    Move to gradle

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -20,12 +20,9 @@ dependencies {
     testImplementation(libs.junit.jupiter)
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    //api(libs.commons.math3)
-
+        
     // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    //implementation(libs.guava)
+    testImplementation(libs.logback.classic)
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
