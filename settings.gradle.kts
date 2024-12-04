3b959aebb48e44bc1ba269d1791a76da48d84207
Will Sargent <will.sargent@gmail.com>
2024-12-04 09:21:25 -0800

    gpg works better

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,5 +10,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "debugjsse-project"
+rootProject.name = "debugjsse"
 include("debugjsse")
